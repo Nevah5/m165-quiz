192b1cdef5311de92a25faf6d61f3bb534eb5d10
Nevah5 <neevaah5@gmail.com>
2022-11-24 09:41:04 +0100

    Initialized springboot backend

---
+++ backend/src/main/java/net/geeler/m165_quiz/M165QuizApplication.java
@@ -0,0 +1,13 @@
+package net.geeler.m165_quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M165QuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M165QuizApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/net/geeler/m165_quiz/M165QuizApplicationTests.java
@@ -0,0 +1,13 @@
+package net.geeler.m165_quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M165QuizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
