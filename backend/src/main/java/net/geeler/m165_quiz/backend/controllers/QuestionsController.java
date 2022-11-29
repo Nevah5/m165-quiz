d2dfb4e91694ad4719543cc05d1313e96641d172
Nevah5 <neevaah5@gmail.com>
2022-11-30 00:03:23 +0100

    Updated QuestionsController

--- backend/src/main/java/net/geeler/m165_quiz/backend/controllers/QuestionsController.java
+++ backend/src/main/java/net/geeler/m165_quiz/backend/controllers/QuestionsController.java
@@ -1,13 +1,24 @@
 package net.geeler.m165_quiz.backend.controllers;
 
+import net.geeler.m165_quiz.backend.models.Question;
+import net.geeler.m165_quiz.backend.repositories.QuestionsRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class QuestionsController {
+    QuestionsRepository questionsRepository;
+    public QuestionsController(QuestionsRepository questionsRepository){
+        this.questionsRepository = questionsRepository;
+    }
     @GetMapping("/question/{id}")
-    public String test(@PathVariable String id){
-        return id;
+    public String getQuestionById(@PathVariable String id){
+        Question foundQuestion = questionsRepository.findQuestionById(id);
+        Double r = Math.floor(Math.random() * 10);
+        if(r >= 0 && r < 2.5) return foundQuestion.getQ1();
+        if(r >= 2.5 && r < 5) return foundQuestion.getQ2();
+        if(r >= 5 && r < 7.5) return foundQuestion.getQ3();
+        return foundQuestion.getQ4();
     }
 }
