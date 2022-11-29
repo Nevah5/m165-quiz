264ce69e02aa8ef93881f3d055181e19905dc216
Nevah5 <neevaah5@gmail.com>
2022-11-29 09:49:03 +0100

    Refactor renamed Controller to QuestionsController

--- backend/src/main/java/net/geeler/m165_quiz/backend/QuestionsController.java
+++ backend/src/main/java/net/geeler/m165_quiz/backend/QuestionsController.java
@@ -1,13 +1,11 @@
 package net.geeler.m165_quiz.backend;
 
-import jakarta.websocket.server.PathParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Controller {
+public class QuestionsController {
     @GetMapping("question/{id}")
     public String test(@PathVariable String id){
         return id;
