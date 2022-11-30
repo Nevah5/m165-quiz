e3a0fcd340576cc86e86472ffdc8f35d342c61db
Nevah5 <neevaah5@gmail.com>
2022-11-30 22:36:40 +0100

    Added QuestionResponse class

---
+++ backend/src/main/java/net/geeler/m165_quiz/backend/models/QuestionResponse.java
@@ -0,0 +1,30 @@
+package net.geeler.m165_quiz.backend.models;
+
+import lombok.Setter;
+
+public class QuestionResponse {
+    @Setter
+    private String id;
+    @Setter
+    private Integer questionNumber;
+    @Setter
+    private String question;
+    @Setter
+    private String answer1;
+    @Setter
+    private String answer2;
+    @Setter
+    private String answer3;
+    @Setter
+    private String answer4;
+
+    public QuestionResponse(Question question){
+        this.id = question.getId();
+        this.questionNumber = question.getQuestionNumber();
+        this.question = question.getQuestion();
+        this.answer1 = question.getAnswer1();
+        this.answer2 = question.getAnswer2();
+        this.answer3 = question.getAnswer3();
+        this.answer4 = question.getAnswer4();
+    }
+}
