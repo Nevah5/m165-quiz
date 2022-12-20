4369df7e06e16918345cbfd3c7bbea2004fa656a
Nevah5 <neevaah5@gmail.com>
2022-12-20 15:25:23 +0100

    Added backend to return leaderboard

--- backend/src/main/java/dev/nevah5/m165_quiz/backend/controllers/GamesController.java
+++ backend/src/main/java/dev/nevah5/m165_quiz/backend/controllers/GamesController.java
@@ -3,9 +3,10 @@ package dev.nevah5.m165_quiz.backend.controllers;
 import com.google.gson.Gson;
 import dev.nevah5.m165_quiz.backend.models.Game;
 import dev.nevah5.m165_quiz.backend.repositories.GamesRepository;
-import dev.nevah5.m165_quiz.backend.repositories.QuestionsRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -17,9 +18,16 @@ public class GamesController {
     }
     @PostMapping(value = "/games", produces = "application/json", consumes = "text/plain")
     @ResponseStatus(HttpStatus.OK)
-    public String getQuestionById(@RequestBody String gameData){
+    public String saveData(@RequestBody String gameData){
         Game g = new Gson().fromJson(gameData, Game.class);
         gamesRepository.save(g);
         return new Gson().toJson(g);
+    }
+
+    @GetMapping(value = "/games/{id}", produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    public String getGamesById(@PathVariable String id){
+        List<Game> games = gamesRepository.getGamesByQuizId(id);
+        return new Gson().toJson(games);
     }
 }
--- backend/src/main/java/dev/nevah5/m165_quiz/backend/repositories/GamesRepository.java
+++ backend/src/main/java/dev/nevah5/m165_quiz/backend/repositories/GamesRepository.java
@@ -4,6 +4,9 @@ import dev.nevah5.m165_quiz.backend.models.Game;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GamesRepository extends MongoRepository<Game, String> {
+    List<Game> getGamesByQuizId(String id);
 }
