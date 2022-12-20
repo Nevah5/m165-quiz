5bad69545d518859e5903707ed37b02492848604
Nevah5 <neevaah5@gmail.com>
2022-12-20 11:00:36 +0100

    Finished storing username in QuizView

--- frontend/src/views/QuizView.vue
+++ frontend/src/views/QuizView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useRoute } from "vue-router";
 import { onMounted, ref } from "vue";
+import router from "@/router";
 
 interface Quiz {
   id: string;
@@ -14,6 +15,7 @@ const route = useRoute();
 const id = route.params.id;
 var data = ref<null | Quiz>();
 const quizFound = ref(true);
+var nameValue = "";
 
 onMounted(() => {
   fetch(`http://localhost:8080/quiz/${id}`)
@@ -23,20 +25,73 @@ onMounted(() => {
     })
     .then((json) => (data.value = json));
 });
+
+const submit = () => {
+  localStorage.setItem("username", nameValue.substring(0, 63));
+  router.push(`/quiz/${id}/play`);
+};
 </script>
 
 <template>
-  <div v-if="quizFound">
+  <main v-if="quizFound">
     <h1>{{ data?.name }}</h1>
-    <p>by {{ data?.createdBy }}</p>
-    <p>{{ new Date(data?.createdOn! * 1000).toLocaleDateString("de-CH") }}</p>
+    <p>
+      by {{ data?.createdBy }} -
+      {{ new Date(data?.createdOn! * 1000).toLocaleDateString("de-CH") }}
+    </p>
 
-    <input type="text" placeholder="Enter your Name" />
-    <button type="submit">Start Quiz</button>
-  </div>
-  <div v-else>
+    <form class="actions" @submit.prevent="submit">
+      <input
+        type="text"
+        placeholder="Enter your Name"
+        maxlength="64"
+        v-model.trim="nameValue"
+      />
+      <button type="submit">Start Quiz</button>
+    </form>
+  </main>
+  <main v-else>
     <h1>This quiz was not found.</h1>
-  </div>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+main {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+main h1 {
+  margin: 0;
+}
+
+main form.actions {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  margin-top: 100px;
+}
+
+main form.actions input {
+  border: solid black 3px;
+  border-radius: 0.5em;
+  height: 40px;
+  padding: 10px;
+}
+
+main form.actions button {
+  height: 40px;
+  background-color: black;
+  color: white;
+  border: none;
+  border-radius: 0.5em;
+  cursor: pointer;
+  padding: 5px 10px;
+  border: solid black 3px;
+  font-weight: bold;
+}
+</style>
